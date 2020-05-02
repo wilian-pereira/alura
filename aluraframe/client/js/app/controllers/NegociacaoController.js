c25874503fd23b6c0bcee2251ab97d0b0e5722f4
Wilian <wilian_pereira1@hotmail.com>
2020-05-02 14:13:23 -0300

    Atualizar modelo

--- aluraframe/client/js/app/controllers/NegociacaoController.js
+++ aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -6,7 +6,8 @@ class NegociacaoController {
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
-        this._listaNegociacoes = new ListaNegociacoes();
+        this._listaNegociacoes = new ListaNegociacoes(model =>
+			this._negociacoesView.update(model));
         
         this._negociacoesView = new NegociacoesView($('#negociacoesView'));
         this._negociacoesView.update(this._listaNegociacoes);
@@ -20,7 +21,6 @@ class NegociacaoController {
         
         event.preventDefault();
         this._listaNegociacoes.adiciona(this._criaNegociacao());
-        this._negociacoesView.update(this._listaNegociacoes);
 		
 		this._mensagem.texto = 'Negociacao adicionada com sucesso!';
 		this._mensagemView.update(this._mensagem);
@@ -43,4 +43,11 @@ class NegociacaoController {
         this._inputValor.value = 0.0;
         this._inputData.focus();   
     }
+	
+	apaga() {
+		this._listaNegociacoes.esvazia();
+
+		this._mensagem.texto = 'Negociações apagadas com sucesso';
+		this._mensagemView.update(this._mensagem);
+	}
 }
